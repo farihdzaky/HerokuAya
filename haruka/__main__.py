a0baa677f74fc5b8758f4ced81e95a5098897873
farihdzaky <79653902+farihdzaky@users.noreply.github.com>
2021-03-07 22:35:16 +0700

    Update __main__.py

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -135,14 +135,14 @@ def send_start(bot, update):
         pass
 
     #chat = update.effective_chat  # type: Optional[Chat] and unused variable
-    text = "Hey there! My name is Haruka Aya - I'm here to help you manage your groups!\n\
+    text = "Hey there! My name is Zenitsu - I'm here to help you manage your groups!\n\
 Click Help button to find out more about how to use me to my full potential.\n\n"
 
-    text += "Join [Faq Group](https://t.me/zainstechchat) ( @zainstechchat ) if you need any support or help\n\n\
-Follow [Zains Projects](https://t.me/zainsprojects) ( @ZainsProjects ) if you want to keep up with the news, updates and bot downtime!\n\n\
-\n\nWant to add me to your group? [Click here!](t.me/HarukaAyaBot?startgroup=true)"
+    text += "Join [Faq Group](https://t.me/ZenitsuFaq) ( @ZenitsuFaq ) if you need any support or help\n\n\
+Follow [Zains Projects](https://t.me/ZenitsuAgatsumaUpdate) ( @ZenitsuAgatsumaUpdate ) if you want to keep up with the news, updates and bot downtime!\n\n\
+\n\nWant to add me to your group? [Click here!](t.me/ZenitusAgatsumaRobot?startgroup=true)"
 
-    keyboard = [[InlineKeyboardButton(text="📢 Support Group", url="https://t.me/HarukaAyaGroup")]]
+    keyboard = [[InlineKeyboardButton(text="📢 Support Group", url="https://t.me/ZenitsuBotSupport")]]
     keyboard += [[InlineKeyboardButton(text="🛠 Control panel", callback_data="cntrl_panel_M")]]
     keyboard += [[InlineKeyboardButton(text="🇺🇸 Language", callback_data="set_lang_"), 
         InlineKeyboardButton(text="❔ Help", callback_data="help_back")]]
